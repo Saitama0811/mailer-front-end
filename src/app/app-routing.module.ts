37d85db4c32e2b5471042fc7b58df428835ca177
saitama0811 <goel.123.saurabh@gmail.com>
2019-02-28 02:52:43 +0530

    (initial_commit)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotpassComponent } from './forgotpass/forgotpass.component';
+import { Error404Component } from './error404/error404.component';
+import { ChangepassComponent } from './changepass/changepass.component';
+import { InboxComponent } from './inbox/inbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login' , component: LoginComponent},
+  {path: 'signup' , component: SignupComponent},
+  {path: 'forgotpass' , component: ForgotpassComponent},
+  {path: '404' , component: Error404Component},
+  {path: 'changepass' , component: ChangepassComponent},
+  {path: '' , redirectTo: '/login', pathMatch: 'full'},
+  {path: 'inbox' , component: InboxComponent},
+  {path: '**' , component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule,  MatCardModule, MatListModule, MatMenuModule,
+    MatToolbarModule, MatSelectModule, MatOptionModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotpassComponent } from './forgotpass/forgotpass.component';
+import { Error404Component } from './error404/error404.component';
+import { ChangepassComponent } from './changepass/changepass.component';
+import { InboxComponent } from './inbox/inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotpassComponent,
+    Error404Component,
+    ChangepassComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
